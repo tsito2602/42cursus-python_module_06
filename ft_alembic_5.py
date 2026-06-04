0c5e2e46da2c370e01ada6e463147cedc80798b7
tsito2602 <white.galaxys2.album2@gmail.com>
2026-06-05 00:07:55 +0900

    typo修正

--- ft_alembic_5.py
+++ ft_alembic_5.py
@@ -2,7 +2,7 @@ from alchemy import create_air
 
 
 def main() -> None:
-    print("=== Alembic 4 ===")
+    print("=== Alembic 5 ===")
     print("Accessing the alchemy module using 'from alchemy import ...'")
     print(f"Testing create_air: {create_air()}")
 
